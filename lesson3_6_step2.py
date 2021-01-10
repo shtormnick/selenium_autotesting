6d305dacf3f9efb68054c06a4fbbaedb9954fdde
shtormnik <foxred324@gmail.com>
2021-01-10 21:42:45 +0200

    add Class

--- lesson3_6_step2.py
+++ lesson3_6_step2.py
@@ -5,31 +5,33 @@ import pytest
 import math 
 import time
 
+class TestSeweralLinks():
+    @pytest.mark.parametrize("link", [("https://stepik.org/lesson/236895/step/1"),
+    ("https://stepik.org/lesson/236896/step/1"),
+    ("https://stepik.org/lesson/236897/step/1"),
+    ("https://stepik.org/lesson/236898/step/1"),
+    ("https://stepik.org/lesson/236899/step/1"),
+    ("https://stepik.org/lesson/236903/step/1"),
+    ("https://stepik.org/lesson/236904/step/1"),
+    ("https://stepik.org/lesson/236905/step/1")])
+    def test_main(self,link):
+        try:
+            browser = webdriver.Chrome()
+            browser.implicitly_wait(5)
+            browser.get(link)
+            answer = math.log(int(time.time()))
+            str_answert = str(answer)
+            
+            input_answer = browser.find_element_by_css_selector(".textarea")
+            input_answer.send_keys(str_answert)
+            submit= browser.find_element_by_class_name("submit-submission")
+            submit.click()
 
-@pytest.mark.parametrize("link", [("https://stepik.org/lesson/236895/step/1"),
-("https://stepik.org/lesson/236896/step/1"),
-("https://stepik.org/lesson/236897/step/1"),
-("https://stepik.org/lesson/236898/step/1"),
-("https://stepik.org/lesson/236899/step/1"),
-("https://stepik.org/lesson/236903/step/1"),
-("https://stepik.org/lesson/236904/step/1"),
-("https://stepik.org/lesson/236905/step/1")])
-def test_main(link):
-    try:
-        browser = webdriver.Chrome()
-        browser.implicitly_wait(5)
-        browser.get(link)
-        answer = math.log(int(time.time()))
-        str_answert = str(answer)
-        
-        input_answer = browser.find_element_by_css_selector(".textarea")
-        input_answer.send_keys(str_answert)
-        submit= browser.find_element_by_class_name("submit-submission")
-        submit.click()
+            correct = browser.find_element_by_class_name("smart-hints__hint").text
+            assert correct == "Correct!" , "something wrong with correct field"
 
-        correct = browser.find_element_by_class_name("smart-hints__hint").text
-        assert correct == "Correct!" , "something wrong with correct field"
-
-    finally:
-        time.sleep(5)
-        browser.quit()
+        finally:
+            time.sleep(5)
+            browser.quit()
+        if __name__ == "__main__":
+            pytest.main()
